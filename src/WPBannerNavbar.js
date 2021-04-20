13af8e6f0f16f93cef693c7c8869b72b9209cfbe
BrandonXLF <brandfowl@gmail.com>
2021-04-20 18:47:33 -0400

    General fixes, load [[Module:Navbar/styles.css]]

--- src/WPBannerNavbar.js
+++ src/WPBannerNavbar.js
@@ -1,27 +1,49 @@
 mw.hook('wikipage.content').add(function($content) {
 	var headers = $content.find('.wpb .wpb-header');
 
-	headers.each(function() {
-		var header = $(this),
-			template = header.next().find('.wpb-banner_name').text(),
-			talkpage = new mw.Title(template).getTalkPage().toString(),
-			view = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(template)),
-			discuss = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(talkpage)),
-			edit = mw.config.get('wgScript') + '?action=edit&title=' + encodeURIComponent(template),
-			navbox = $('<div class="navbar plainlinks hlist navbar-mini" style="float:right;">').append(
-				$('<ul>').append(
-					$('<li class="nv-view">').append($('<a>').attr('href', view).append($('<abbr>').attr('title', 'View this template').text('v'))),
-					$('<li class="nv-view">').append($('<a>').attr('href', discuss).append($('<abbr>').attr('title', 'Discuss this template').text('t'))),
-					$('<li class="nv-edit">').append($('<a>').attr('href', edit).append($('<abbr>').attr('title', 'Edit this templatee').text('e')))
-				)
-			);
+	function addNavbar() {
+		headers.each(function() {
+			var header = $(this),
+				template = header.next().find('.wpb-banner_name').text(),
+				talkpage = new mw.Title(template).getTalkPage().toString(),
+				view = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(template)),
+				discuss = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(talkpage)),
+				edit = mw.config.get('wgScript') + '?action=edit&title=' + encodeURIComponent(template),
+				navbar = $('<div class="navbar plainlinks hlist navbar-mini" style="float:right;">').append(
+					$('<ul>').append(
+						$('<li class="nv-view">').append($('<a>')
+							.attr('href', view)
+							.append('<abbr title="View this template">v</attr>')
+						),
+						$('<li class="nv-talk">').append($('<a>')
+							.attr('href', discuss)
+							.append('<abbr title="Discuss this template">t</attr>')
+						),
+						$('<li class="nv-edit">').append($('<a class="external text">')
+							.attr('href', edit)
+							.append('<abbr title="Edit this template">e</attr>')
+						)
+					)
+				);
 
-		if (header.css('display') === 'none') {
-			header.next().find('> :first-child .mbox-text').first().prepend(navbox).children().css('clear', 'both');
-			navbox.css('margin-right', '1px');
-		} else {
-			header.find('> :nth-child(2)').append(navbox);
-			navbox.css('margin', '.15em .5em 0 0');
-		}
-	});
+			if (header.css('display') === 'none') {
+				header.next().find('> :first-child .mbox-text').first().prepend(navbar).children().css('clear', 'both');
+				navbar.css('margin-right', '1px');
+				return;
+			}
+
+			header.find('> :nth-child(2)').append(navbar);
+			navbar.css('margin', '.15em .5em 0 0');
+		});
+	}
+
+	if (headers.length !== 0) {
+		var link = document.createElement('link');
+
+		link.setAttribute('rel', 'stylesheet');
+		link.setAttribute('href', 'https://en.wikipedia.org/wiki/Module:Navbar/styles.css?action=raw&ctype=text/css');
+		link.addEventListener('load', addNavbar);
+
+		document.head.appendChild(link);
+	}
 });

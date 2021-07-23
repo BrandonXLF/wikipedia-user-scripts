61053bf648935aad6a9ee1fe9ffcb86bc6134374
BrandonXLF <brandfowl@gmail.com>
2021-07-23 02:18:05 -0400

    Only expand references with URLs and only the URL

--- src/ReferenceExpander.js
+++ src/ReferenceExpander.js
@@ -9,15 +9,6 @@
 
 $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 	e.preventDefault();
-	var progress = new OO.ui.ProgressBarWidget({
-			progress: false
-		}),
-		doing = $('<div style="margin:0.5em 0;font-weight:bold;">Initalizing...</div>'),
-		logElement = $('<div style="font-family:monospace monospace;"></div>');
-
-	function log(msg, color) {
-		logElement.append($('<div>').text('> ' + msg).css({color: color || 'grey'}));
-	}
 
 	function ProcessDialog(config) {
 		ProcessDialog.super.call(this, config);
@@ -26,7 +17,7 @@ $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 	OO.inheritClass(ProcessDialog, OO.ui.ProcessDialog);
 
 	ProcessDialog.static.name = 'citoidExpandRefs';
-	ProcessDialog.static.title = 'Reference Expanders';
+	ProcessDialog.static.title = 'Reference Expander';
 	ProcessDialog.static.actions = [{
 		action: 'close',
 		label: 'Close',
@@ -35,12 +26,21 @@ $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 
 	ProcessDialog.prototype.initialize = function() {
 		ProcessDialog.super.prototype.initialize.apply(this, arguments);
+
 		this.content = new OO.ui.PanelLayout({
 			padded: true,
 			expanded: false
 		});
-		processDialog.content.$element.append(progress.$element, doing, logElement);
+		this.progressBar = new OO.ui.ProgressBarWidget({
+			progress: false
+		});
+		this.logElement = $('<div>').css('font-family', 'monospace monospace');
+
+		this.progressBar.$element.css({marginTop: '1em'});
+		this.content.$element.append(this.logElement, this.progressBar.$element);
 		this.$body.append(this.content.$element);
+
+		this.setStatus('Initalizing...');
 	};
 
 	ProcessDialog.prototype.getActionProcess = function() {
@@ -54,6 +54,20 @@ $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 		return this.content.$element.outerHeight(true);
 	};
 
+	ProcessDialog.prototype.log = function(msg, color) {
+		this.logElement.append($('<div>').text('> ' + msg).css({color: color || 'grey'}));
+		this.updateSize();
+		this.$body.scrollTop(this.$body.prop('scrollHeight'));
+	};
+
+	ProcessDialog.prototype.setStatus = function(text) {
+		this.title.setLabel(ProcessDialog.static.title + ': ' + text);
+	};
+
+	ProcessDialog.prototype.setProgress = function(progress) {
+		this.progressBar.setProgress(progress);
+	};
+
 	var windowManager = new OO.ui.WindowManager();
 
 	$(document.body).append(windowManager.$element);
@@ -65,34 +79,41 @@ $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 	windowManager.addWindows([processDialog]);
 	windowManager.openWindow(processDialog);
 
-	log('Loading page content and scripts...');
+	processDialog.log('Loading page content and scripts...');
 
 	mw.loader.getScript('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/Citoid.js&action=raw&ctype=text/javascript').then(function() {
 		new mw.Api().edit(mw.config.get('wgPageName'), function(rev) {
-			var def = $.Deferred(),
-				refs = rev.content.match(/<ref.*?>.*?<\/ref>/g),
+			var urlProtocols = mw.config.get('wgUrlProtocols'),
+				urlProtocolsWithoutRel = mw.config.get('wgUrlProtocols').split('|').filter(function(protocol) {
+					return protocol !== '\\/\\/';
+				}).join('|'),
+				urlCharacters = '[^<>"\\x00-\\x20\\x7F\\xA0\\u1680\\u2000-\\u200A\\u202F\\u205F\\u3000\\uFFFD]', // From Parser::EXT_LINK_URL_CLASS
+				enclosedUrlRegex = new RegExp('\\[((?:' + urlProtocols + ')' + urlCharacters + '*).?\\]'),
+				unenclosedUrlRegex = new RegExp('((?:' + urlProtocolsWithoutRel + ')' + urlCharacters + '*)'),
+				refRegex = /<ref(?:[^>]+?[^/]|)>.*?<\/ref>/g,
+				textarea = document.createElement('textarea'),
+				def = $.Deferred(),
+				refs = rev.content.match(refRegex),
 				done = 0;
 
-			progress.setProgress(0);
-			doing.text('Expanding references...');
+			processDialog.setProgress(0);
+			processDialog.setStatus('Expanding references...');
 
 			function afterRef(msg, color) {
 				done++;
 
-				log(msg, color);
-				progress.setProgress(done/refs.length*100);
-				processDialog.updateSize();
+				processDialog.log(msg, color);
+				processDialog.setProgress(done / refs.length * 100);
 
 				if (done >= refs.length) {
-					progress.setProgress(false);
-					doing.text('Saving...');
-					log('Saving changes to ' + mw.config.get('wgPageName') + '...');
-					processDialog.updateSize();
+					processDialog.setProgress(false);
+					processDialog.setStatus('Saving...');
+					processDialog.log('Saving changes to ' + mw.config.get('wgPageName') + '...');
 
 					var pos = 0;
 
 					def.resolve({
-						text: rev.content.replace(/<ref.*?>.*?<\/ref>/g, function() {
+						text: rev.content.replace(refRegex, function() {
 							return refs[pos++];
 						}),
 						summary: 'Expanding bare references using [[User:BrandonXLF/ReferenceExpander|ReferenceExpander]]'
@@ -100,38 +121,75 @@ $(mw.util.addPortletLink('p-tb', '#', 'Expand references')).click(function(e) {
 				}
 			}
 
-			function doRef(i) {
-				if (refs[i].match(/<ref.*?> *{{/)) {
+			function expandRef(startTag, url, endTag, refIndex) {
+				getCitoidRef(url).then(function(expanded) {
+					refs[refIndex] = startTag + expanded + endTag;
+					afterRef('Expanded reference to "' + url + '".', 'green');
+				}, function() {
+					afterRef('Error expanding reference to "' + url + '".', 'red');
+				});
+			}
+
+			function doRef(refIndex) {
+				if (refs[refIndex].match(/<ref.*?> *{{/)) {
 					afterRef('Skipping already expanded reference.');
 					return;
 				}
 
-				var parts = refs[i].match(/(<ref.*?>)(.*?)(<\/ref>)/),
-					unexpanded = parts[2].trim().match(/\[?([^ \]]*)*\]?/)[1].trim();
+				var parts = refs[refIndex].match(/(<ref.*?>)(.*?)(<\/ref>)/),
+					startTag = parts[1],
+					refText = parts[2].trim(),
+					endTag = parts[3],
+					match;
 
-				getCitoidRef(unexpanded).then(function(expanded) {
-					refs[i] = parts[1] + expanded + parts[3];
-					afterRef('Expanded reference to "' + unexpanded + '".', 'green');
-				}, function() {
-					afterRef('Error expanding reference to "' + unexpanded + '".', 'red');
-				});
+				// Unescape HTML escape codes
+				textarea.innerHTML = refText;
+				refText = textarea.value;
+
+				// Match url in brackets
+				match = refText.match(enclosedUrlRegex);
+
+				if (match) {
+					expandRef(startTag, match[1], endTag, refIndex);
+					return;
+				}
+
+				// Match url out of brackets
+				match = refText.match(unenclosedUrlRegex);
+
+				if (match) {
+					// Remove trailing punctuation
+					// From Parser::makeFreeExternalLink
+					var sep = ',;.:!?';
+					if (match[1].indexOf('(') == -1) sep += ')';
+
+					var trailLength = 0;
+
+					for (var i = match[1].length - 1; i >= 0; i--) {
+						if (sep.indexOf(match[1][i]) == -1) break;
+						else trailLength++;
+					}
+
+					expandRef(startTag, match[1].substring(0, match[1].length - trailLength), endTag, refIndex);
+					return;
+				}
+
+				afterRef('Skipped reference without URL.');
+				return;
 			}
 
 			if (refs) {
 				for (var i = 0; i < refs.length; i++) doRef(i);
 			} else {
-				log('No references found on the page.');
-				def.resolve({
-					text: rev.content
-				});
+				processDialog.log('No references found on the page.');
+				def.resolve({text: rev.content});
 			}
 
 			return def.promise();
 		}).then(function() {
-			progress.setProgress(100);
-			doing.text('Edit saved!');
-			log('Edit saved. Reloading...');
-
+			processDialog.setProgress(100);
+			processDialog.setStatus('Edit saved!');
+			processDialog.log('Edit saved. Reloading...');
 			window.location.reload();
 		});
 	});

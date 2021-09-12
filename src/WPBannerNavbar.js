786896dca6b9cda1629f56b3cc5f8752c0bd927a
BrandonXLF <brandfowl@gmail.com>
2021-09-12 16:44:19 -0400

    Check if template name is not found or is invalid

--- src/WPBannerNavbar.js
+++ src/WPBannerNavbar.js
@@ -3,28 +3,41 @@ mw.hook('wikipage.content').add(function(content) {
 
 	function addNavbar() {
 		headers.each(function() {
-			var header = $(this),
-				template = header.next().find('.wpb-banner_name').text(),
-				talkpage = new mw.Title(template).getTalkPage().toString(),
-				view = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(template)),
-				discuss = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(talkpage)),
-				edit = mw.config.get('wgScript') + '?action=edit&title=' + encodeURIComponent(template),
-				navbar = $('<div class="navbar plainlinks hlist navbar-mini" style="float:right;">').append(
-					$('<ul>').append(
-						$('<li class="nv-view">').append($('<a>')
-							.attr('href', view)
-							.append('<abbr title="View this template">v</attr>')
-						),
-						$('<li class="nv-talk">').append($('<a>')
-							.attr('href', discuss)
-							.append('<abbr title="Discuss this template">t</attr>')
-						),
-						$('<li class="nv-edit">').append($('<a class="external text">')
-							.attr('href', edit)
-							.append('<abbr title="Edit this template">e</attr>')
-						)
+			var header, template, title, view, discuss, edit, navbar;
+
+			header = $(this);
+			template = header.next().find('.wpb-banner_name').text();
+
+			if (!template) {
+				return;
+			}
+
+			try {
+				title = new mw.Title(template);
+			} catch (e) {
+				return;
+			}
+
+			view = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(template));
+			discuss = mw.config.get('wgArticlePath').replace('$1', encodeURIComponent(title.getTalkPage().getPrefixedDb()));
+			edit = mw.config.get('wgScript') + '?action=edit&title=' + encodeURIComponent(template);
+
+			navbar = $('<div class="navbar plainlinks hlist navbar-mini" style="float:right;">').append(
+				$('<ul>').append(
+					$('<li class="nv-view">').append($('<a>')
+						.attr('href', view)
+						.append('<abbr title="View this template">v</attr>')
+					),
+					$('<li class="nv-talk">').append($('<a>')
+						.attr('href', discuss)
+						.append('<abbr title="Discuss this template">t</attr>')
+					),
+					$('<li class="nv-edit">').append($('<a class="external text">')
+						.attr('href', edit)
+						.append('<abbr title="Edit this template">e</attr>')
 					)
-				);
+				)
+			);
 
 			if (header.css('display') === 'none') {
 				header.next().find('> :first-child .mbox-text').first().prepend(navbar).children().css('clear', 'both');

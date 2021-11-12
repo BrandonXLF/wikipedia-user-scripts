b148d1e6df411b6d8f3438e2822b7b93ae6b7a25
BrandonXLF <brandfowl@gmail.com>
2021-11-12 16:53:42 -0500

    Use em for height and mark as important to override MobileFrontend

--- src/NoRedirect.js
+++ src/NoRedirect.js
@@ -25,7 +25,7 @@ mw.hook('wikipage.content').add(function(content) {
 						.attr('alt', 'no redirect')
 						.attr('src', 'data:image/svg+xml,<svg%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20width%3D"16"%20height%3D"14"%20viewBox%3D"0%200%2016%2014"%20version%3D"1.1"><path%20d%3D"m%201%2C0%20v%208.5%20c%200%2C1.04%201.02%2C1.98%202.02%2C1.98%20h%206%20l%203%2C0.02"%20style%3D"fill%3Anone%3Bstroke%3A%23000000%3Bstroke-width%3A2%3B"%2F><path%20d%3D"m%209.5%2C7%200.02%2C7%206.5%2C-3.5%20z"%20id%3D"path4"%2F><%2Fsvg>')
 						.css({
-							height: '8px',
+							height: '0.6em !important',
 							verticalAlign: 'text-top'
 						})
 				);

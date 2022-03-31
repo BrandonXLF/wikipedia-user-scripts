ab8963ab75f8173fde9ded72a969d01b10ace01a
BrandonXLF <42590338+BrandonXLF@users.noreply.github.com>
2022-03-30 22:17:05 -0400

    Use mw.Title to handle titles

--- src/SubpageMover.js
+++ src/SubpageMover.js
@@ -22,12 +22,20 @@ $(function() {
 		return $('<a>').attr('href', mw.util.getUrl(page)).text(page);
 	};
 
-	Log.prototype.logFailure = function(from, to, reasonHtml) {
-		this.log(['Failed to move page ', this.createLink(from), ' to ', this.createLink(to), '. Reason: ', reasonHtml], '#d33');
+	Log.prototype.logError = function(html) {
+		this.log(html, '#d33');
 	};
 
-	Log.prototype.logSuccess = function(from, to) {
-		this.log(['Successfully moved page ', this.createLink(from), ' to ', this.createLink(to), '.'], '#14866d');
+	Log.prototype.logSuccess = function(html) {
+		this.log(html, '#14866d');
+	};
+
+	Log.prototype.logMoveError = function(from, to, reasonHtml) {
+		this.logError(['Failed to move page ', this.createLink(from), ' to ', this.createLink(to), '. Reason: ', reasonHtml]);
+	};
+
+	Log.prototype.logMoveSuccess = function(from, to) {
+		this.logSuccess(['Successfully moved page ', this.createLink(from), ' to ', this.createLink(to), '.']);
 	};
 
 	function movePage(from, to, params, log, onSuccess) {
@@ -43,22 +51,22 @@ $(function() {
 			errorlang: 'uselang'
 		}, params)).done(function(response) {
 			if (response.errors) {
-				log.logFailure(from, to, response.errors[0].html);
+				log.logMoveError(from, to, response.errors[0].html);
 				return;
 			}
 
-			log.logSuccess(response.move.from, response.move.to);
+			log.logMoveSuccess(response.move.from, response.move.to);
 
 			if (response.move['talkmove-errors']) {
 				var talkFrom = (from.match(':') ? from.replace(':', ' talk:') : 'Talk:') + from,
 					talkTo = (to.match(':') ? to.replace(':', ' talk:') : 'Talk:') + to;
 
-				log.logFailure(talkFrom, talkTo, response.move['talkmove-errors'][0].html);
+				log.logMoveError(talkFrom, talkTo, response.move['talkmove-errors'][0].html);
 				return;
 			}
 
 			if (response.move.talkfrom) {
-				log.logSuccess(response.move.talkfrom, response.move.talkto);
+				log.logMoveSuccess(response.move.talkfrom, response.move.talkto);
 			}
 
 			onSuccess && onSuccess();
@@ -75,11 +83,8 @@ $(function() {
 			return;
 		}
 
-		var fromTitle = $('input[name="wpOldTitle"]').val(),
-			toNamespaceId = $('select[name="wpNewTitleNs"]').val(),
-			toNamespace = mw.config.get('wgFormattedNamespaces')[toNamespaceId].replace(' ', '_'),
-			toPage = $('input[name=wpNewTitleMain]').val(),
-			toTitle = (toNamespace === '' ? '' : toNamespace) + toPage,
+		var fromTitle = new mw.Title($('input[name="wpOldTitle"]').val()),
+			toTitle = mw.Title.makeTitle($('select[name="wpNewTitleNs"]').val(), $('input[name="wpNewTitleMain"]').val()),
 			params = {
 				reason: $('input[name="wpReason"]').val(),
 				movetalk: $('input[name="wpMovetalk"]').prop('checked') ? true : undefined,
@@ -87,21 +92,27 @@ $(function() {
 				watchlist: $('input[name="wpWatch"]').prop('checked') ? 'watch' : 'nochange',
 			};
 
+		if (!toTitle) {
+			log.logError('New title is an invalid page!');
+			return;
+		}
+
 		$.get(mw.config.get('wgScriptPath') + '/api.php', {
 			action: 'query',
 			list: 'allpages',
-			apprefix: fromTitle + '/',
+			apprefix: fromTitle.getMainText() + '/',
+			apnamespace: fromTitle.getNamespaceId(),
 			pslimit: '500',
 			format: 'json',
 			formatversion: '2'
 		}).done(function(res) {
-			movePage(fromTitle, toTitle, params, log, function() {
-				var prefixRegex = new RegExp('^' + mw.util.escapeRegExp(fromTitle));
+			movePage(fromTitle.getPrefixedText(), toTitle.getPrefixedText(), params, log, function() {
+				var prefixRegex = new RegExp('^' + mw.util.escapeRegExp(fromTitle.getPrefixedText()));
 
 				res.query.allpages.forEach(function(info) {
 					if (info.title === fromTitle) return;
 
-					movePage(info.title, info.title.replace(prefixRegex, toTitle), params, log);
+					movePage(info.title, info.title.replace(prefixRegex, toTitle.getPrefixedText()), params, log);
 				});
 			});
 		});

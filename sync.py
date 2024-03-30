8a953c32f685aa9046f5fccecc3e9cd3d960b6ac
Brandon Fowler <brandfowl@gmail.com>
2024-03-30 03:56:22 -0400

    Remove GitHub workflow
    IP address is blocked from editing

---
+++ sync.py
@@ -0,0 +1,72 @@
+import requests
+import os
+import subprocess
+
+USERNAME = 'BrandonXLF'
+PASS = input("Enter password for BrandonXLF: ")
+
+print("Stashing uncommitted changes")
+subprocess.call(['git', 'stash', '--include-untracked'])
+
+s = requests.Session()
+
+r = s.get('https://en.wikipedia.org/w/api.php', params={
+	'action': 'query',
+	'meta': 'tokens',
+	'type': 'login',
+	'format': 'json'
+})
+
+print('Got login token')
+
+login = r.json()['query']['tokens']['logintoken']
+
+r = s.post('https://en.wikipedia.org/w/api.php', data={
+	'action': 'login',
+	'lgname': USERNAME,
+	'lgpassword': PASS,
+	'format': 'json',
+	'lgtoken': login
+})
+
+print('Logged into English Wikipedia')
+
+r = s.get('https://en.wikipedia.org/w/api.php', params={
+	'action': 'query',
+	'meta': 'tokens',
+	'type': 'csrf',
+	'format': 'json'
+})
+
+csrf = r.json()['query']['tokens']['csrftoken']
+
+print('Got edit token')
+
+def edit_file(file, text):
+	req = s.get('https://en.wikipedia.org/w/index.php', params={
+		'action': 'raw',
+		'title': 'User:' + USERNAME + '/' + file
+	})
+	
+	if req.ok and req.text == text:
+		print('Skipped file ' + file)
+		return
+	
+	res = s.post('https://en.wikipedia.org/w/api.php', data={
+		'action': 'edit',
+		'title': 'User:' + USERNAME + '/' + file,
+		'token': csrf,
+		'text': text,
+		'summary': input("Enter summary for edit to {}: ", file),
+		'format': 'json'
+	})
+	
+	print('Saved file ' + file + ': ' + res.text)
+
+for subdir, dirs, files in os.walk(os.getcwd() + '/src'):
+	for file in files:
+		with open(subdir + os.sep + file, encoding='utf8') as f:
+			edit_file(file, f.read())
+
+print("Popping stash")
+subprocess.call(['git', 'stash', 'pop'])
